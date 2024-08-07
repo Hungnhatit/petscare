276cdaa4d2574716bb81149be9a6e63e7636a6cc
Hungnhatit <nhatpro204@gmail.com>
2024-08-07 15:12:57 +0700

    update admin login controller

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -5,65 +5,66 @@ namespace App\Http\Controllers;
 use function Laravel\Prompts\Alert;
 use App\Models\CuaHang;
 use Illuminate\Http\Request;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
-    public function getStoreLogin()
-    {
-        return view('login.store');
+  public function getStoreLogin()
+  {
+    return view('login.store');
+  }
+  public function postLogin(Request $request)
+  {
+
+    $arr = [
+      'USERNAME' => $request->name,
+      'password' => $request->password, // Laravel yêu cầu key là 'password'
+    ];
+
+    if (Auth::guard('administrator')->attempt($arr)) {
+      $request->session()->put('store', Auth::guard('administrator')->user()); // Lưu thông tin cửa hàng vào session
+      return redirect()->intended('/');
+    } else {
+      return redirect()->back()->with('status', 'Tên đăng nhập hoặc mật khẩu không chính xác');
     }
-    public function postLogin(Request $request)
-    {
+  }
 
-        $arr = [
-            'USERNAME' => $request->name,
-            'password' => $request->password, // Laravel yêu cầu key là 'password'
-        ];
+  public function logout(Request $request)
+  {
+    // Chức năng đăng xuất
+    Auth::guard('administrator')->logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
 
-        if (Auth::guard('administrator')->attempt($arr)) {
-            $request->session()->put('store', Auth::guard('administrator')->user()); // Lưu thông tin cửa hàng vào session
-            return redirect()->intended('/');
-        } else {
-            return redirect()->back()->with('status', 'Tên đăng nhập hoặc mật khẩu không chính xác');
-        }
-    }
+    return redirect()->route('login');
+  }
 
-    public function logout(Request $request)
-    {
-        Auth::guard('administrator')->logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+  public function index()
+  {
+    $stores = CuaHang::all();
+    return view('managements.dashboard', compact('stores'));
+  }
 
-        return redirect()->route('login');
-    }
+  public function edit($id)
+  {
+    $stores = new CuaHang();
+    $store = $stores::where('IDCH', $id)->get()->toArray();
+    dd($store[0]['IDCH']);
+  }
 
-    public function index()
-    {
-        $stores = CuaHang::all();
-        return view('managements.dashboard', compact('stores'));
-    }
+  public function update(Request $request, $id)
+  {
+    $store = CuaHang::where('IDCH', $id)
+      ->where('TRANGTHAI', 'Chờ duyệt')
+      ->firstOrFail();
+    $store->TRANGTHAI = 'Đã duyệt';
+    $store->save();
+    return redirect('/account')->with('success', 'Player created successfully.');
+  }
 
-    public function edit($id)
-    {
-        $stores = new CuaHang();
-        $store = $stores::where('IDCH', $id)->get()->toArray();
-        dd($store[0]['IDCH']);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $store = CuaHang::where('IDCH', $id)
-            ->where('TRANGTHAI', 'Chờ duyệt')
-            ->firstOrFail();
-        $store->TRANGTHAI = 'Đã duyệt';
-        $store->save();
-        return redirect('/account')->with('success', 'Player created successfully.');
-    }
-
-    public function destroy($id)
-    {
-        CuaHang::where('IDCH', $id)->delete();
-        return redirect('/account')->with('success', 'Member deleted successfully.');
-    }
+  public function destroy($id)
+  {
+    CuaHang::where('IDCH', $id)->delete();
+    return redirect('/account')->with('success', 'Member deleted successfully.');
+  }
 }
